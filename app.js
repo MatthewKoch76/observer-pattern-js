936bc6a60f47f7f670ac51f4d0c017abc91e13c3
MatthewKoch76 <37314071+MatthewKoch76@users.noreply.github.com>
2019-04-04 10:03:09 -0500

    finished

---
+++ app.js
@@ -0,0 +1,53 @@
+function eventObserver() {
+  this.observers = [];
+}
+
+eventObserver.prototype = {
+  subscribe: function(fn){
+    this.observers.push(fn);
+    console.log(`you are now subscribed to ${fn.name}`);
+  },
+  unsubscribe: function(fn){
+    this.observers = this.observers.filter(function(item){
+      if(item !== fn){
+        return item
+      }
+    });
+    console.log(`you are now unsubscribed from ${fn.name}`);
+  },
+  fire: function(){
+    this.observers.forEach(function(item){
+    item.call();
+    })
+  }
+}
+
+const click = new eventObserver();
+
+document.querySelector('.sub-ms').addEventListener('click', function(){
+  click.subscribe(getCurMilliseconds)
+});
+
+document.querySelector('.unsub-ms').addEventListener('click', function(){
+  click.unsubscribe(getCurMilliseconds)
+});
+
+document.querySelector('.sub-s').addEventListener('click', function(){
+  click.subscribe(getCurSeconds)
+});
+
+document.querySelector('.unsub-s').addEventListener('click', function(){
+  click.unsubscribe(getCurSeconds)
+});
+
+document.querySelector('.fire').addEventListener('click', function(){
+  click.fire();
+});
+
+const getCurMilliseconds = function() {
+  console.log(`Current Milliseconds: ${new Date().getMilliseconds()}`);
+}
+
+const getCurSeconds = function() {
+  console.log(`Current Seconds: ${new Date().getSeconds()}`);
+}
---
+++ appes6.js
@@ -0,0 +1,55 @@
+class eventObserver {
+  constructor() {
+    this.observers = [];
+  }
+  
+  subscribe(fn) {
+    this.observers.push(fn);
+    console.log(`you are now subscribed to ${fn.name}`);
+  }
+
+  unsubscribe(fn) {
+    this.observers = this.observers.filter(function(item){
+      if(item !== fn){
+        return item
+      }
+    });
+    console.log(`you are now unsubscribed from ${fn.name}`);
+  }
+
+  fire() {
+    this.observers.forEach(function(item){
+      item.call();
+      })
+  }
+}
+
+const click = new eventObserver();
+
+document.querySelector('.sub-ms').addEventListener('click', function(){
+  click.subscribe(getCurMilliseconds)
+});
+
+document.querySelector('.unsub-ms').addEventListener('click', function(){
+  click.unsubscribe(getCurMilliseconds)
+});
+
+document.querySelector('.sub-s').addEventListener('click', function(){
+  click.subscribe(getCurSeconds)
+});
+
+document.querySelector('.unsub-s').addEventListener('click', function(){
+  click.unsubscribe(getCurSeconds)
+});
+
+document.querySelector('.fire').addEventListener('click', function(){
+  click.fire();
+});
+
+const getCurMilliseconds = function() {
+  console.log(`Current Milliseconds: ${new Date().getMilliseconds()}`);
+}
+
+const getCurSeconds = function() {
+  console.log(`Current Seconds: ${new Date().getSeconds()}`);
+}
